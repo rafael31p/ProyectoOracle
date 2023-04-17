d81ccd9c7d1ab439da32db5c97f0515eeda32988
rafael31p <rafael31p@gmail.com>
2023-04-16 19:41:41 -0500

    Se generan cambios en el css, index.html y en el js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const noMessage = document.getElementById('noMessage');
+const instruction = document.getElementById('instruction');
 const inputText = document.getElementById('inputText');
 const outputText = document.getElementById('outputText');
 const encryptBtn = document.getElementById('encryptBtn');
@@ -8,17 +10,30 @@ const encrypt = text => text.replaceAll('e', 'enter').replaceAll('i', 'imes').re
 
 const decrypt = text => text.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
 
+function updateMessages() {
+    if (outputText.value === '') {
+        noMessage.style.display = 'block';
+        instruction.style.display = 'block';
+    } else {
+        noMessage.style.display = 'none';
+        instruction.style.display = 'none';
+    }
+}
+
 encryptBtn.addEventListener('click', () => {
     const text = inputText.value.toLowerCase();
     outputText.value = encrypt(text);
+    updateMessages(); // Agregar esta línea
 });
 
 decryptBtn.addEventListener('click', () => {
     const text = inputText.value.toLowerCase();
     outputText.value = decrypt(text);
+    updateMessages(); // Agregar esta línea
 });
 
 copyBtn.addEventListener('click', () => {
     outputText.select();
     document.execCommand('copy');
 });
+
